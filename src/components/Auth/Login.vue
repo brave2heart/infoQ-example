898695a7c2e447294d9ed63ae71b4ffd164c4188
weihao <1120575939@qq.com>
2020-04-19 19:22:44 +0800

    add login page setting

---
+++ src/components/Auth/Login.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="login-page">
+    <span>
+      <el-button @click="loginClicked">登录</el-button>
+      <el-button>注册</el-button>
+    </span>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {}
+  },
+  methods: {
+    loginClicked () {
+      this.$router.push('/Home')
+    }
+  }
+}
+</script>
+<style scoped>
+.login-page {
+  display: flex;
+  justify-self: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  background-color: cornflowerblue;
+}
+</style>
---
+++ src/components/Container/Home.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <el-button @click="addClicked">添加设备</el-button>
+    <el-dialog  title="添加设备" :visible.sync="addVisible">
+      <div>
+        <el-table :data="data">
+          <el-table-column prop="index" label="序号">
+          </el-table-column>
+          <el-table-column prop="id" label="设备id"></el-table-column>
+        </el-table>
+      </div>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      data: [],
+      addVisible: false
+    }
+  },
+  created () {
+    for (let index = 0; index < 10; index++) {
+      this.data.push({
+        index: index,
+        id: 'xxxxxxx'
+      })
+    }
+  },
+  methods: {
+    addClicked () {
+      this.addVisible = true
+    }
+  }
+}
+</script>
+<style>
+.el-header {
+  background-color: #b3c0d1;
+  color: #333;
+  line-height: 60px;
+}
+
+.el-aside {
+  color: #333;
+}
+</style>
